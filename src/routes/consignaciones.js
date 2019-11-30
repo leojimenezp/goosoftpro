77da5e754bc036e3d717c87b4021831c8053003b
sebastianbedoya <47502521+sebastianbedoya@users.noreply.github.com>
2019-11-30 01:35:57 -0500

    funcional todo
    funcional faltaron detalles

--- src/routes/consignaciones.js
+++ src/routes/consignaciones.js
@@ -134,9 +134,6 @@ router.post('/AsignarConsignacionSola', isLoggedIn, async (req,res) =>{
     res.redirect('/consignaciones');
 })
 
-
-
-
 router.get('/consignaciones/DetallesDeCostoToltal/:id_consignacion/:id',isLoggedIn, async (req,res) => {
     const { id_consignacion } = req.params;
     const { id } = req.params;
@@ -176,51 +173,121 @@ router.get('/consignaciones/EliminarItemSola/:id/:id_consignacion',isLoggedIn, a
     
      console.log('esta es la consignacion',id_consignacion,'este es el id',id)
 
-
-    res.redirect(`/consignaciones/DetallesDeCostoTotal/${id_consignacion}/${id}`);
+    res.redirect('/consignaciones')
 })
 
+router.post('/editarestado123',isLoggedIn, async (req,res) => {
 
-
-
-//************************************************************************************************ */
-//************************************************************************************************ */
-//************************************************************************************************ */
-//************************************************************************************************ */
-//************************************************************************************************ */
-//************************************************************************************************ */
-//************************************************************************************************ */
-//************************************************************************************************ */
-//************************************************************************************************ */
-router.get('/consignaciones/EliminarConsignacionSola/:id_consignacion',isLoggedIn, async (req,res) => {
-const tb_equipo_item_personal = await pool.query(`SELECT ie.id_equipo_item_personal, ie.id_planeacion, ie.cantidad, ie.costo, m.abreviatura_moneda, c.nombre_cargo, p.nombre_personal, p.apellido_personal, u.abreviatura_unidad_medida, r.sigla_rubro, p.bono_salarial_personal, (cantidad * costo) + p.bono_salarial_personal total_costo, id_mov_item_personal, ((ie.fecha_final_mov - ie.fecha_inicio_mov) + (ie.fecha_final_demov - ie.fecha_inicio_demov)) dias, (((fecha_final_mov - fecha_inicio_mov) + (fecha_final_demov - fecha_inicio_demov))*ROUND(p.salario_personal / 30)) + p.bono_salarial_personal total FROM tb_equipo_item_personal ie, tb_cargos c, tb_personal p, tb_unidad_medida u, tb_rubros r, tb_monedas m WHERE ie.id_cargo = c.id_cargo AND ie.id_personal = p.id AND ie.id_unidad_medida = u.id_unidad_medida AND ie.id_rubro = r.id_rubro AND ie.id_moneda = m.id_moneda AND ie.id_planeacion = '${id_planeacion}'`);
-res.redirect('/consignaciones');
-})
-
-router.get('/consignaciones/AsiganarConsignacionSola/:id_consignacion',isLoggedIn, async (req,res) => {
-    
-    const consulta = await pool.query( `select * from tb_planeacion` );
-    const consulta1 = await pool.query( `select * from tb_consignacion`);
-
-    res.render('consignaciones/asignar-consignacio',{
-        consulta:consulta,
-        consulta1:consulta1
-    });
-})
-router.post('/AsiganarConsignacionSola1',isLoggedIn, async (req,res) => {
-    
-    const { id_planeacion} = req.body;
-    const { planeacion} = req.body;
-    const { id_consignacion} = req.body;
-    console.log(id_planeacion)
-    console.log(planeacion)
-    console.log(id_consignacion)
-
-    const consulta = await pool.query( `UPDATE tb_consignacion SET 
-    id_planeacion='${id_planeacion}', planeacion='${ planeacion}' WHERE id_consignacion='${id_consignacion}'` );
+    const { estado } = req.body;
+    const { descripcion } = req.body;
+    const { id_consignacion } = req.body;
+    console.log('estado:',{estado},'descripcion:',{descripcion},'id_consigacion:',{id_consignacion})
+    const consulta = await pool.query( ` UPDATE tb_consignacion SET estado='${estado}',descripcion='${descripcion}'  WHERE id_consignacion='${id_consignacion}'` );
     
     res.redirect('/consignaciones');
 }) 
+
+
+//************************************************************************************************ */
+//************************************************************************************************ */
+//************************************************************************************************ */
+//************************************************************************************************ */
+//************************************************************************************************ */
+//************************************************************************************************ */
+//************************************************************************************************ */
+//************************************************************************************************ */
+//************************************************************************************************ */
+1
+
+
+
+
+router.get('/consignaciones/DetallesPlaneacion/:id_planeacion',isLoggedIn, async (req,res) => {
+    const {id_planeacion}= req.params;
+    const tb_equipo_item_personal = await pool.query(`
+    SELECT p.id, ie.id_equipo_item_personal, ie.id_planeacion, ie.cantidad, ie.costo, m.abreviatura_moneda, c.nombre_cargo, p.nombre_personal, p.apellido_personal, u.abreviatura_unidad_medida, r.sigla_rubro, p.bono_salarial_personal, (cantidad * costo) + p.bono_salarial_personal total_costo, id_mov_item_personal, ((ie.fecha_final_mov - ie.fecha_inicio_mov) + (ie.fecha_final_demov - ie.fecha_inicio_demov)) dias, (((fecha_final_mov - fecha_inicio_mov) + (fecha_final_demov - fecha_inicio_demov))*ROUND(p.salario_personal / 30)) + p.bono_salarial_personal total 
+    FROM tb_equipo_item_personal ie, tb_cargos c, tb_personal p, tb_unidad_medida u, tb_rubros r, tb_monedas m 
+    WHERE ie.id_cargo = c.id_cargo 
+    AND ie.id_personal = p.id 
+    AND ie.id_unidad_medida = u.id_unidad_medida 
+    AND ie.id_rubro = r.id_rubro 
+    AND ie.id_moneda = m.id_moneda 
+    AND ie.id_planeacion = '${id_planeacion}'`);
+
+    const consulta1= await pool.query(`SELECT *FROM tb_consignacion c , tb_personal p WHERE c.id_personal=p.id AND c.id_planeacion='${id_planeacion}'`);
+    res.render('consignaciones/detalles-planeacion',
+    {
+        tb_equipo_item_personal:tb_equipo_item_personal,
+        consulta1:consulta1
+    });
+    })
+    router.get('/consignacionesconplaneacion/agregar/:id/:id_planeacion', isLoggedIn, async (req,res) => {
+        
+        const {id}= req.params;
+        const {id_planeacion}= req.params;
+        const consulta = await pool.query(`select * from tb_personal WHERE id = ${id}`);
+        
+        res.render('consignaciones/asignar-consignacio-planeacion',{
+            consulta:consulta,
+            id_planeacion:id_planeacion
+        });
+    })
+    router.post('/AgregarItemplaneacion', isLoggedIn, async (req,res) =>{
+   
+        const {id_personal} = req.body;
+        const {fecha} = req.body;
+        const {codigo} = req.body;
+        const {estado} = req.body;
+        const {id_planeacion} = req.body;
+        const datos={id_personal,fecha,codigo,estado,id_planeacion} 
+        await pool.query( "INSERT INTO tb_consignacion SET ?", [datos] );
+        const consulta = await pool.query( `select * from tb_personal WHERE id='${id_personal}' `);
+        const consulta1 = await pool.query( `select * from tb_consignacion WHERE codigo='${codigo}' `);
+        const consulta3 = await pool.query( `select * from tb_consignacion WHERE codigo='${codigo}' `); 
+        res.render('consignaciones/agregar-personal-externa-planeacion',{
+        consulta:consulta,
+        consulta1:consulta1,
+        consulta3:consulta3 
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
