bd243992170ba5356272e89bc42a890dc6f0f0f5
winigalviz <galvizwinifred@gmail.com>
2019-12-21 15:23:53 -0500

    hoja de trabajo

--- src/index.js
+++ src/index.js
@@ -84,6 +84,7 @@ app.use(require('./routes/planeacion_template'));
 app.use(require('./routes/consignaciones'));
 app.use(require('./routes/legalizacion'));
 app.use(require('./routes/gestionbonos'));
+app.use(require('./routes/hojas-trabajo'));
 
 // Public
 app.use(express.static(path.join(__dirname, 'public')));
